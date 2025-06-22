339557f0966a4e865b4f7effe7f8e982de3985f6
Cap Labib <79649995+caplabib@users.noreply.github.com>
2025-06-22 17:28:53 +0600

    v1.1.0

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,55 @@
-const searchBtn = document.querySelector('#js-search-btn');
+const searchBtn = document.querySelector("#js-search-btn");
 
-searchBtn.addEventListener('click', () => {
-  console.log('hello world');
+const inputCity = document.querySelector("#cityNameInput");
+const dateText = document.querySelector("#dateText");
+const cityNameText = document.querySelector("#cityName");
+const weatherImg = document.querySelector("#weatherImg");
+const weatherText = document.querySelector("#weatherText");
+const weatherTempText = document.querySelector("#weatherTempText");
+const highestTemp = document.querySelector("#highestTempText");
+const lowestTemp = document.querySelector("#lowestTempText");
+
+let date = new dayjs();
+
+searchBtn.addEventListener("click", () => {
+  console.log("fatching");
+  getWeatherData();
+});
+inputCity.addEventListener("keydown", (event) => {
+  if (event.key === "Enter" && inputCity.value !== null || undefined) {
+    getWeatherData();
+    
+  }
+});
+
+async function getWeatherData() {
+  inputCity.blur()
   
-})
+  let cityName = inputCity.value;
+  
+  let fetchData = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${
+      cityName || "Dhaka"
+    }&appid=4fa744968212805aba43213504b39bd1&units=metric`,
+    {
+      headers: {
+        Accept: "applicaiton/json",
+      },
+    }
+  );
+
+  let weather = await fetchData.json();
+  console.log(weather);
+
+  dateText.textContent = date.format("dddd, D MMMM YYYY");
+  cityNameText.textContent = weather.name;
+  weatherImg.src = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
+  weatherText.textContent = weather.weather[0].main;
+  weatherTempText.textContent = `${Math.floor(weather.main.temp)}°C`;
+  highestTemp.textContent = `${weather.main.temp_max}°C`;
+  lowestTemp.textContent = `${weather.main.temp_min}°C`;
+
+  console.log("done");
+}
+
+window.addEventListener("DOMContentLoaded", getWeatherData);
